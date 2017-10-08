1f4c44b936d16a83ef0d643451d6cf58a5de6c30
Madhur <madhur.ahuja@helpchat.in>
2017-10-08 16:12:10 +0530

    refactor to component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumDetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
+
     render() {
-
-
+        return (
+            <View>
+                <Text>{this.props.album.title}</Text>
+            </View>
+        );
     }
 }
+
+export default AlbumDetail;

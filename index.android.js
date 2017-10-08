9504adc2c0c8c5232a22b8c6e95ce676f20f6647
Madhur <madhur.ahuja@helpchat.in>
2017-10-08 12:16:17 +0530

    http request

--- index.android.js
+++ index.android.js
@@ -1,1 +1,23 @@
-// Index.android.js - place code in here for Android
+// Index.ios.js - Place code here in for ios
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+
+const App = () => {
+  return (
+    <View>
+      <Header headerText={'Albums123'} />
+      <AlbumList />
+    </View>
+
+  );
+};
+
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);

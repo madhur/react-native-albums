d4788e162edd9519b4ca53280d9a9874c7418858
Madhur <madhur.ahuja@helpchat.in>
2017-10-08 16:25:21 +0530

    card component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
 
 class AlbumDetail extends Component {
 
@@ -10,9 +11,9 @@ class AlbumDetail extends Component {
 
     render() {
         return (
-            <View>
+            <Card>
                 <Text>{this.props.album.title}</Text>
-            </View>
+            </Card>
         );
     }
 }

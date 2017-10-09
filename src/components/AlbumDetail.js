908bf43a601a77a7787f49c21cd388039b978960
Madhur <madhur.ahuja@helpchat.in>
2017-10-09 15:50:14 +0530

    added album artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -17,13 +17,16 @@ class AlbumDetail extends Component {
 		return (
 			<Card>
 				<CardSection>
-					<View>
+					<View style={styles.thumbnailContainerStyle}>
 						<Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
 					</View>
 					<View style={styles.headerContentStyle}>
-						<Text>{title}</Text>
+						<Text style={styles.headerTextStyle}>{title}</Text>
 						<Text>{artist}</Text>
 					</View>
+				</CardSection>
+				<CardSection>
+					<Image style={styles.imageStyle} source={{ uri: this.props.album.image }} />
 				</CardSection>
 			</Card>
 		);
@@ -35,9 +38,24 @@ const styles = {
 		flexDirection: 'column',
 		justifyContent: 'space-around'
 	},
+	headerTextStyle: {
+		fontSize: 18
+	},
 	thumbnailStyle: {
 		height: 50,
 		width: 50
+	},
+	thumbnailContainerStyle: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 10,
+		marginRight: 10
+
+	},
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
 	}
 };
 

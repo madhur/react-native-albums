037edc6c471145e7fec1191a3410f7bea468c657
Madhur <madhur.ahuja@helpchat.in>
2017-10-09 16:09:46 +0530

    button access

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 class Button extends Component {
+    
 
     render() {
+        const { buttonStyle, textStyle } = styles;
+
         return (
-            <TouchableOpacity>
-                <Text>Click Me!</Text>
+            <TouchableOpacity style={buttonStyle}>
+                <Text style={textStyle}>Click Me!</Text>
             </TouchableOpacity>
         );
     }
 }
+
+const styles = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingBottom: 10,
+        paddingTop: 10
+    },
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+    }
+};
 
 export default Button;
